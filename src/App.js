6fdde2fbcaa914e1d832a322f59221baf95c42c5
Matamir <mhirsch217@gmail.com>
2023-05-02 15:21:47 -0400

    CSS for all suits, and enums for card values.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './Cards/Card.js';
+import Card from './Cards/Card';
+import { CardData, CardSuit } from './Cards/enums.ts';
 
 function App() {
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <Card Suit="Diamond" Value="K"></Card>
+          <Card Suit={CardSuit.Spade} Value={CardData.King}></Card>
+          <br></br>
+          <Card Suit={CardSuit.Diamond} Value={CardData.Queen}></Card>
+          <br></br>
+          <Card Suit={CardSuit.Club} Value={CardData.Jack}></Card>
+          <br></br>
+          <Card Suit={CardSuit.Heart} Value={CardData.Ace}></Card>
         </header>
       </div>
     </>
--- src/Cards/Card.js
+++ src/Cards/Card.js
@@ -1,38 +1,41 @@
 import { DEBUGGER } from "../Debugging";
 import { CardValue } from "./enums.ts";
 import "./Card.css";
+
 export function Card({ Value, Suit }) {
 
-    let LocValue = Value;
-    let LocSuit = Suit;
+    let LocValue = Value.Display;
+    let LocSuit = Suit.Suit;
+    let LocColor = Suit.Color;
 
     ////////////////////////////////////////////////////////////////////////
-    
+
     DEBUGGER(CardValue.valueOf(LocValue))
 
-    // Sanatize inputs
-    if (String(LocValue) !== LocValue) {
-        DEBUGGER("Card Value:" + LocValue + "Not A String")
-        return
-    }
+    // // Sanatize inputs
+    // if (String(LocValue) !== LocValue) {
+    //     DEBUGGER("Card Value:" + LocValue + "Not A String")
+    //     return
+    // }
 
-    if (String(LocSuit) !== LocSuit) {
-        DEBUGGER("Card Suit:" + LocSuit + "Not A String")
-        return
-    }
+    // if (String(LocSuit) !== LocSuit) {
+    //     DEBUGGER("Card Suit:" + LocSuit + "Not A String")
+    //     return
+    // }
 
-    if ((LocSuit !== "Spade") && (LocSuit !== "Club") && (LocSuit !== "Diamond") && (LocSuit !== "Red")) {
-        DEBUGGER("Invalid Suit" + LocSuit)
-        return
-    }
+    // if ((LocSuit !== "Spade") && (LocSuit !== "Club") && (LocSuit !== "Diamond") && (LocSuit !== "Heart")) {
+    //     DEBUGGER("Invalid Suit" + LocSuit)
+    //     return
+    // }
 
-    if ((LocValue != 1)) {}
+    // if ((LocValue != 1)) { }
 
     ////////////////////////////////////////////////////////////////////////
 
 
 
-    let Color = (LocSuit === "Spade" || LocSuit === "Club") ? "Black" : "Red";
+    let Color = LocColor;
+    //let Color = (LocSuit === "Spade" || LocSuit === "Club") ? "Black" : "Red";
 
     let Spade =
         <div className="spade">
@@ -41,12 +44,50 @@ export function Card({ Value, Suit }) {
             <span className="spade3" />
         </div>
 
+    let Club =
+        <div className="club">
+            <span className="club1" />
+            <span className="club2" />
+            <span className="club3" />
+            <span className="club4" />
+        </div>
+
     let Heart =
         <div className="heart">
             <span className="heart1" />
             <span className="heart2" />
         </div>
 
-    return <>{Heart}{Spade}<h1>   {LocValue}.{LocSuit}.{Color}</h1 ></>
+    let Diamond =
+        <div className="diamond">
+            <span className="diamond1" />
+            <span className="diamond2" />
+        </div>
+
+    let SuitDisplay;
+
+    switch (LocSuit) {
+        case "Spade":
+            SuitDisplay = Spade;
+            break;
+        case "Heart":
+            SuitDisplay = Heart;
+            break;
+        case "Club":
+            SuitDisplay = Club;
+            break;
+        case "Diamond":
+            SuitDisplay = Diamond;
+            break;
+        default:
+
+    }
+
+    return (
+        <div className="card">
+            {SuitDisplay}
+            <h1>{LocValue}</h1>
+        </div>
+    )
 
 } export default Card;
